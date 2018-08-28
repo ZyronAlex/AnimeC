ba0bac784f046384df7b4c04bb60a05dcc8f7d04
AlexFBelo <fraga040506>
2018-08-28 09:22:17 -0300

    Finalizando o PlayVideo

--- app/src/main/java/com/example/play/MyPlayerActivity.java
+++ app/src/main/java/com/example/play/MyPlayerActivity.java
@@ -1,24 +1,16 @@
 package com.example.play;
 
 import android.content.Intent;
-import android.content.res.Resources;
 import android.net.Uri;
-import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.SeekBar;
 
-import com.androidquery.AQuery;
 import com.example.chromecast.ExpandedControlsActivity;
 import com.example.item.ItemLatest;
 import com.example.util.Utils;
@@ -30,16 +22,14 @@ import com.google.android.gms.cast.framework.CastSession;
 import com.google.android.gms.cast.framework.SessionManagerListener;
 import com.google.android.gms.cast.framework.media.RemoteMediaClient;
 import com.google.android.gms.common.images.WebImage;
+import com.halilibo.bettervideoplayer.BetterVideoCallback;
 import com.halilibo.bettervideoplayer.BetterVideoPlayer;
 import com.tecapps.AnimeC.R;
 
-import java.util.Timer;
-
-import static com.example.play.MyPlayerActivity.PlaybackLocation.REMOTE;
 import static com.example.play.MyPlayerActivity.PlaybackState.IDLE;
 
 
-public class MyPlayerActivity extends AppCompatActivity implements MyBetterVideoCallback {
+public class MyPlayerActivity extends AppCompatActivity implements BetterVideoCallback {
 
     private static final String TAG = "MyPlayerActivity";
     private BetterVideoPlayer player;
@@ -47,19 +37,8 @@ public class MyPlayerActivity extends AppCompatActivity implements MyBetterVideo
     private MenuItem mediaRouteMenuItem;
     private CastSession mCastSession;
     private PlaybackState mPlaybackState;
-    private PlaybackLocation mLocation;
     private ItemLatest mSelectedMedia;
-    private AQuery mAquery;
-    private Fragment castMiniController;
     private SessionManagerListener<CastSession> mSessionManagerListener;
-
-    /**
-     * indicates whether we are doing a local or a remote playback
-     */
-    public enum PlaybackLocation {
-        LOCAL,
-        REMOTE
-    }
 
     /**
      * List of various states that we can be in
@@ -84,9 +63,6 @@ public class MyPlayerActivity extends AppCompatActivity implements MyBetterVideo
         // Grabs a reference to the player view
         player = (BetterVideoPlayer) findViewById(R.id.player);
 
-        //Set Immediately starts playback when the player becomes prepared.
-//        player.setAutoPlay(true);
-
         //Enable SwipeGestures
         player.enableSwipeGestures(this.getWindow());
 
@@ -94,17 +70,10 @@ public class MyPlayerActivity extends AppCompatActivity implements MyBetterVideo
         player.setCallback(this);
 
         // Sets the function of Chrome Cast SDK
-        mAquery = new AQuery(this);
         setupCastListener();
         mCastContext = CastContext.getSharedInstance(this);
         mCastSession = mCastContext.getSessionManager().getCurrentCastSession();
 
-//        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-//                RelativeLayout.LayoutParams.FILL_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-//        castMiniController = new Fragment();
-//        castMiniController.setUserVisibleHint(false);
-//        castMiniController.getClass().a
-//        player.addView();
 
         // see what we need to play and where
         Bundle bundle = this.getIntent().getExtras();
@@ -114,16 +83,14 @@ public class MyPlayerActivity extends AppCompatActivity implements MyBetterVideo
             Log.d(TAG, "Setting url of the VideoView to: " + mSelectedMedia.getVideoUrl());
             // To play files, you can use Uri.fromFile(new File("..."))
             player.setSource(Uri.parse(mSelectedMedia.getVideoUrl()));
+
             if (mCastSession != null && mCastSession.isConnected()) {
-                // this will be the case only if we are coming from the
-                // CastControllerActivity by disconnecting from a device
-                updatePlaybackLocation(PlaybackLocation.LOCAL);
-                player.start();
-            } else {
                 // we should load the video but pause it
                 // and show the album art.
-                updatePlaybackLocation(PlaybackLocation.REMOTE);
                 loadRemoteMedia(player.getCurrentPosition(), true);
+            } else {
+                // Set Immediately starts playback when the player becomes prepared.
+                player.setAutoPlay(true);
             }
         }
 
@@ -162,11 +129,6 @@ public class MyPlayerActivity extends AppCompatActivity implements MyBetterVideo
         Log.d(TAG, "onResume() was called");
         mCastContext.getSessionManager().addSessionManagerListener(
                 mSessionManagerListener, CastSession.class);
-        if (mCastSession != null && mCastSession.isConnected()) {
-            updatePlaybackLocation(PlaybackLocation.REMOTE);
-        } else {
-            updatePlaybackLocation(PlaybackLocation.LOCAL);
-        }
         super.onResume();
     }
 
@@ -181,7 +143,6 @@ public class MyPlayerActivity extends AppCompatActivity implements MyBetterVideo
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Intent intent;
         if (item.getItemId() == android.R.id.home) {
             ActivityCompat.finishAfterTransition(this);
             onBackPressed();
@@ -243,24 +204,6 @@ public class MyPlayerActivity extends AppCompatActivity implements MyBetterVideo
 
     }
 
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-    }
-
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-        player.pause();
-    }
-
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
-        if (mPlaybackState == PlaybackState.PLAYING) {
-            play(seekBar.getProgress());
-        } else if (mPlaybackState != PlaybackState.IDLE) {
-            player.seekTo(seekBar.getProgress());
-        }
-    }
-
 
     //Methods for Chrome Cast SDK
     private void setupCastListener() {
@@ -310,24 +253,18 @@ public class MyPlayerActivity extends AppCompatActivity implements MyBetterVideo
             private void onApplicationConnected(CastSession castSession) {
                 mCastSession = castSession;
                 if (null != mSelectedMedia) {
-
                     if (mPlaybackState == PlaybackState.PLAYING) {
                         player.pause();
                         loadRemoteMedia(player.getCurrentPosition(), true);
                         finish();
                         return;
-                    } else {
-                        mPlaybackState = IDLE;
-                        updatePlaybackLocation(REMOTE);
                     }
                 }
                 supportInvalidateOptionsMenu();
             }
 
             private void onApplicationDisconnected() {
-                updatePlaybackLocation(PlaybackLocation.LOCAL);
                 mPlaybackState = IDLE;
-                mLocation = PlaybackLocation.LOCAL;
                 supportInvalidateOptionsMenu();
             }
         };
@@ -341,41 +278,13 @@ public class MyPlayerActivity extends AppCompatActivity implements MyBetterVideo
         movieMetadata.addImage(new WebImage(Uri.parse(mSelectedMedia.getImageUrl())));
         movieMetadata.addImage(new WebImage(Uri.parse(mSelectedMedia.getVideoImgBig())));
 
+
         return new MediaInfo.Builder(mSelectedMedia.getVideoUrl())
                 .setStreamType(MediaInfo.STREAM_TYPE_BUFFERED)
-                .setContentType("videos/mp4")
+                .setContentType(mSelectedMedia.getVideoType())
                 .setMetadata(movieMetadata)
-                .setStreamDuration(Integer.parseInt(mSelectedMedia.getDuration()) * 1000)
+                .setStreamDuration(player.getDuration() * 1000)
                 .build();
-    }
-
-    private void play(int position) {
-        switch (mLocation) {
-            case LOCAL:
-                player.setInitialPosition(position);
-                player.start();
-                break;
-            case REMOTE:
-                mPlaybackState = PlaybackState.BUFFERING;
-                mCastSession.getRemoteMediaClient().seek(position);
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void updatePlaybackLocation(PlaybackLocation location) {
-        mLocation = location;
-        if (location == PlaybackLocation.LOCAL) {
-            if (mPlaybackState == PlaybackState.PLAYING
-                    || mPlaybackState == PlaybackState.BUFFERING) {
-                setCoverArtStatus(null);
-            } else {
-                setCoverArtStatus(mSelectedMedia.getImageUrl());
-            }
-        } else {
-            setCoverArtStatus(mSelectedMedia.getImageUrl());
-        }
     }
 
     private void loadRemoteMedia(int position, boolean autoPlay) {
@@ -418,11 +327,6 @@ public class MyPlayerActivity extends AppCompatActivity implements MyBetterVideo
         remoteMediaClient.load(buildMediaInfo(), autoPlay, position);
     }
 
-    private void setCoverArtStatus(String url) {
-        if (url != null)
-            mAquery.id(player).image(url);
-    }
-
     private void setupActionBar() {
         Toolbar toolbar = player.getToolbar();
         toolbar.setTitle(mSelectedMedia.getVideoName());
@@ -434,7 +338,7 @@ public class MyPlayerActivity extends AppCompatActivity implements MyBetterVideo
         Log.d(TAG, "Controls: PlayBackState: " + state);
         boolean isConnected = (mCastSession != null)
                 && (mCastSession.isConnected() || mCastSession.isConnecting());
-        if(isConnected)
+        if (isConnected)
             player.hideControls();
         else
             player.showControls();
